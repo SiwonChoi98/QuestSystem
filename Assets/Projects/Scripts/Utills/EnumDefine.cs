64815be214532a1740881a84f4487b4643271f62
최시원 <bestvalue0410@naver.com>
2024-03-22 12:10:34 +0900

    [Feat] 퀘스트,보상 타입 추가

---
+++ Assets/Projects/Scripts/Utills/EnumDefine.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum QuestType
+{
+    DestroyEnemy,
+    AcquireItem
+}
+
+public enum RewardType
+{
+    Exp,
+    Item,
+    Gold
+}
